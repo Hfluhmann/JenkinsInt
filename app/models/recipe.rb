1ebb8075151c1fd5318f8fdcec64ae484d29b64d
Valeria Miranda <valeria.mirandam@sansano.usm.cl>
2022-12-07 21:35:31 -0300

    uploading images feature

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
     validates :title, presence: true, length: {minimum: 2} 
     validates :ingredients, presence: true, length: {minimum: 5} 
-    validates :instructions, presence: true, length: {minimum: 5} 
+    validates :instructions, presence: true, length: {minimum: 5}
+    has_one_attached :image
 end
